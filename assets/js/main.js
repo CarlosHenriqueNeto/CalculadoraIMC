de399e73869e55bbc9593db5387d04a1beae433e
Carlos <chn-neto@hotmail.com>
2024-09-01 13:12:11 -0300

    alteração na logica dos Ifs

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,55 +6,46 @@ function calcularIMC() {
     function recebeEventoForm(evento) {
 
         evento.preventDefault();
-
         let peso = form.querySelector('.peso');
         let altura = form.querySelector('.altura');
         peso = Number(peso.value);
         altura = Number(altura.value);
+        let [imc, grau] = logicaIMC(peso, altura);
 
+        resultado.innerHTML = `Seu IMC é ${imc.toFixed(2)} (${grau})`;
+
+    }
+
+    function logicaIMC(peso, altura) {
         let imc = peso / ((altura / 100) * (altura / 100));
         let grau;
 
+        if (imc > 40) {
+            grau = "Obesidade Grau 3";
+            return [imc, grau];
+        }
+        if (imc > 35) {
+            grau = "Obesidade Grau 2";
+            return [imc, grau];
+        }
+        if (imc > 30) {
+            grau = "Obesidade Grau 1";
+            return [imc, grau];
+        }
+        if (imc > 25) {
+            grau = "Sobrepeso";
+            return [imc, grau];
+        }
+        if (imc > 18.6) {
+            grau = "Peso Normal";
+            return [imc, grau];
+        }
         if (imc < 18.5) {
             grau = "Abaixo do Peso";
-
-
+            return [imc, grau];
         }
-        if (18.6 < imc && imc < 24.9) {
-            grau = "Peso Normal";
-
-
-        }
-        if (25 < imc && imc < 29.9) {
-            grau = "Sobrepeso";
-
-
-        }
-        if (30 < imc && imc < 34.9) {
-            grau = "Obresidade Grau 1";
-
-
-        }
-        if (35 < imc && imc < 39.9) {
-            grau = "Obresidade Grau 2";
-
-
-        }
-        if (40 < imc) {
-            grau = "Obresidade Grau 3";
-
-
-        }
-        console.log(imc, grau);
-        resultado.innerHTML = `Seu IMC é ${imc.toFixed(2)} (${grau})`;
-
-
-
 
     }
-    form.addEventListener('submit', recebeEventoForm);
-
-
-
+    form.addEventListener('submit',recebeEventoForm);
 }
 calcularIMC();
